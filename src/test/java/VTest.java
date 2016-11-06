dba7ea3535baef79f49a433a19a8d10fe5d7fbca
Christian Garbs <mitch@cgarbs.de>
2016-11-06 17:42:57 +0100

    add tests for V class

---
+++ src/test/java/VTest.java
@@ -0,0 +1,48 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.collection.IsArrayContainingInOrder.arrayContaining;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import de.cgarbs.wavefront.V;
+
+public class VTest
+{
+	@Test
+	public void sameValuesAreEqual()
+	{
+		V v1 = new V(1, 2, 3);
+		V v2 = new V(1, 2, 3);
+		assertThat(v1, is(v2));
+	}
+
+	@Test
+	public void sameInstancesAreEqual()
+	{
+		V v = new V(1, 2, 3);
+		assertThat(v, is(v));
+	}
+
+	@Test
+	public void differentValuesAreNotEqual()
+	{
+		V v1 = new V(1, 2, 3);
+		V v2 = new V(3, 2, 1);
+		assertThat(v1, not(v2));
+	}
+
+	@Test
+	public void sortsByColumn()
+	{
+		V v1 = new V(2, 2, 1);
+		V v2 = new V(0, 5, 2);
+		V v3 = new V(1, 1, 2);
+		V v4 = new V(0, 2, 3);
+		V[] arr = new V[] { v1, v2, v3, v4 };
+		Arrays.sort(arr);
+		assertThat(arr, arrayContaining(v4, v2, v3, v1));
+	}
+}
