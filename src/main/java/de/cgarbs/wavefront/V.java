217c729c0f79d91b8f86e53365d89524aa58c1a6
Christian Garbs <mitch@cgarbs.de>
2016-11-06 17:42:44 +0100

    implement Comparable

--- src/main/java/de/cgarbs/wavefront/V.java
+++ src/main/java/de/cgarbs/wavefront/V.java
@@ -5,13 +5,14 @@
 package de.cgarbs.wavefront;
 
 /**
- * V is shorthand vor Vertex, a point in 3D space. 
+ * V is shorthand vor Vertex, a point in 3D space.
  * 
  * @author Christian Garbs <mitch@cgarbs.de>
  * @since 0.1.0
  *
  */
-public class V {
+public class V implements Comparable<V>
+{
 
 	final double x;
 	final double y;
@@ -20,24 +21,64 @@ public class V {
 	/**
 	 * Creates a new vertex with the given coordinates.
 	 * 
-	 * @param x x coordinate
-	 * @param y y coordinate
-	 * @param z z coordinate
+	 * @param x
+	 *            x coordinate
+	 * @param y
+	 *            y coordinate
+	 * @param z
+	 *            z coordinate
 	 * @since 0.1.0
 	 */
-	public V(double x, double y, double z) {
+	public V(double x, double y, double z)
+	{
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 
 	@Override
-	public boolean equals(Object o) {
-		if (! (o instanceof V)) {
+	public int compareTo(V that)
+	{
+		int c = compare(this.z, that.z);
+		if (c == 0)
+		{
+			c = compare(this.y, that.y);
+			if (c == 0)
+			{
+				compare(this.x, that.x);
+			}
+		}
+		return c;
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (!(o instanceof V))
+		{
 			return false;
 		}
 		V v = (V) o;
-		return this.x == v.x && this.y == v.y && this.z == v.z;
+		return this == v || (this.x == v.x && this.y == v.y && this.z == v.z);
 	}
-	
+
+	@Override
+	public String toString()
+	{
+		return String.format("V[%.0f %.0f %.0f]", x, y, z);
+	}
+
+	private int compare(double d1, double d2)
+	{
+		if (d2 > d1)
+		{
+			return 1;
+		}
+		if (d2 < d1)
+		{
+			return -1;
+		}
+		return 0;
+	}
+
 }
