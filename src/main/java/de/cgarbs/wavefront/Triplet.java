6ad96f75d4e936e9e711e42a615ac596a51b0e4c
Christian Garbs <mitch@cgarbs.de>
2016-11-08 00:17:56 +0100

    extract Triplet base class

---
+++ src/main/java/de/cgarbs/wavefront/Triplet.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2016 (C)  Christian Garbs <mitch@cgarbs.de>
+ * Licensed under GNU GPL 3 (or later)
+ */
+package de.cgarbs.wavefront;
+
+abstract class Triplet implements Comparable<Triplet>
+{
+
+	protected final double x;
+	protected final double y;
+	protected final double z;
+	private String shorthand;
+
+	protected Triplet(double x, double y, double z, String shorthand)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.shorthand = shorthand;
+	}
+
+	@Override
+	public int compareTo(Triplet that)
+	{
+		int c = compare(this.z, that.z);
+		if (c == 0)
+		{
+			c = compare(this.y, that.y);
+			if (c == 0)
+			{
+				compare(this.x, that.x);
+			}
+		}
+		return c;
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o)
+		{
+			return true;
+		}
+		if (!this.getClass().equals(o.getClass()))
+		{
+			return false;
+		}
+		Triplet v = (Triplet) o;
+		return this.x == v.x && this.y == v.y && this.z == v.z;
+	}
+
+	@Override
+	public String toString()
+	{
+		return String.format("%s[%.0f %.0f %.0f]", shorthand, x, y, z);
+	}
+
+	private int compare(double d1, double d2)
+	{
+		if (d2 > d1)
+		{
+			return 1;
+		}
+		if (d2 < d1)
+		{
+			return -1;
+		}
+		return 0;
+	}
+
+}
--- src/main/java/de/cgarbs/wavefront/V.java
+++ src/main/java/de/cgarbs/wavefront/V.java
@@ -11,12 +11,8 @@ package de.cgarbs.wavefront;
  * @since 0.1.0
  *
  */
-public class V implements Comparable<V>
+public class V extends Triplet
 {
-
-	final double x;
-	final double y;
-	final double z;
 
 	/**
 	 * Creates a new vertex with the given coordinates.
@@ -31,58 +27,7 @@ public class V implements Comparable<V>
 	 */
 	public V(double x, double y, double z)
 	{
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
-
-	@Override
-	public int compareTo(V that)
-	{
-		int c = compare(this.z, that.z);
-		if (c == 0)
-		{
-			c = compare(this.y, that.y);
-			if (c == 0)
-			{
-				compare(this.x, that.x);
-			}
-		}
-		return c;
-	}
-
-	@Override
-	public boolean equals(Object o)
-	{
-		if (this == o)
-		{
-			return true;
-		}
-		if (!(o instanceof V))
-		{
-			return false;
-		}
-		V v = (V) o;
-		return this.x == v.x && this.y == v.y && this.z == v.z;
-	}
-
-	@Override
-	public String toString()
-	{
-		return String.format("V[%.0f %.0f %.0f]", x, y, z);
-	}
-
-	private int compare(double d1, double d2)
-	{
-		if (d2 > d1)
-		{
-			return 1;
-		}
-		if (d2 < d1)
-		{
-			return -1;
-		}
-		return 0;
+		super(x, y, z, "V");
 	}
 
 }
