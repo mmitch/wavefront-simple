20422fd6b8ec30ece2d2cafb86143301b71c53ba
Christian Garbs <mitch@cgarbs.de>
2016-11-20 00:47:36 +0100

    add tests for Obj.apply()

--- src/test/java/de/cgarbs/wavefront/ObjTest.java
+++ src/test/java/de/cgarbs/wavefront/ObjTest.java
@@ -5,6 +5,8 @@
 package de.cgarbs.wavefront;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
@@ -15,6 +17,7 @@ import java.util.List;
 import org.junit.Test;
 
 import de.cgarbs.wavefront.meta.ArgSupplier;
+import de.cgarbs.wavefront.op.Translation;
 import de.cgarbs.wavefront.test.util.NullOutputStream;
 
 public class ObjTest
@@ -33,6 +36,61 @@ public class ObjTest
 
 		// then
 		assertThat(captureList, hasSize(1));
+	}
+
+	@Test
+	public void operationsGetAppliedToEveryFace()
+	{
+		// given
+		Face face1 = new Face(//
+				new V(1, 2, 3), //
+				new V(2, 6, 8), //
+				new V(-10, 4, -3) //
+		);
+		Face face2 = new Face(//
+				new V(-1, -2, 3), //
+				new V(2, -6, -8), //
+				new V(10, 4, 3) //
+		);
+
+		Obj obj = new Obj();
+		obj.addFace(face1);
+		obj.addFace(face2);
+
+		Translation tl = new Translation(new Vec(0.5, 1, 2));
+
+		// when
+		Obj newObj = obj.apply(tl);
+
+		// then
+		assertThat(obj.faces.get(0), is(face1));
+		assertThat(obj.faces.get(1), is(face2));
+		assertThat(newObj.faces.get(0), is(face1.apply(tl)));
+		assertThat(newObj.faces.get(1), is(face2.apply(tl)));
+	}
+
+	@Test
+	public void applyCreatesCopyAndDoesNotChangeOriginalObject()
+	{
+		// given
+		Face face = new Face(//
+				new V(1, 2, 3), //
+				new V(2, -6, -8), //
+				new V(-10, 4, -3) //
+		);
+
+		Obj obj = new Obj();
+		obj.addFace(face);
+
+		Translation tl = new Translation(new Vec(0.5, 1, 2));
+
+		// when
+		Obj newObj = obj.apply(tl);
+
+		// then
+		assertThat(obj.faces.get(0), sameInstance(face));
+		assertThat(newObj, not(sameInstance(obj)));
+		assertThat(newObj.faces.get(0), not(sameInstance(face)));
 	}
 
 	@Test
