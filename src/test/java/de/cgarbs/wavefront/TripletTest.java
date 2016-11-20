3c674357275a55f0924d6edb9f3116d33f87519d
Christian Garbs <mitch@cgarbs.de>
2016-11-20 23:42:26 +0100

    add tests for Triplet equality

---
+++ src/test/java/de/cgarbs/wavefront/TripletTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2016 (C)  Christian Garbs <mitch@cgarbs.de>
+ * Licensed under GNU GPL 3 (or later)
+ */
+package de.cgarbs.wavefront;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class TripletTest
+{
+	@Test
+	public void tripletIsNeverEqualToAnotherClass()
+	{
+		Triplet triplet = new TestableTriplet(1, 2, 3);
+		Coordinate coordinate = new Coordinate(1, 2, 3);
+		assertThat(triplet, not(coordinate));
+	}
+
+	@Test
+	public void tripletEqualsTripletWithSameCoordinates()
+	{
+		Triplet t1 = new TestableTriplet(1, 2, 3);
+		Triplet t2 = new TestableTriplet(1, 2, 3);
+		assertThat(t1, is(t2));
+	}
+
+	@Test
+	public void tripletDoesNotEqualTripletWithDifferentXCoordinate()
+	{
+		Triplet t1 = new TestableTriplet(1, 2, 3);
+		Triplet t2 = new TestableTriplet(-1, 2, 3);
+		assertThat(t1, not(t2));
+	}
+
+	@Test
+	public void tripletDoesNotEqualTripletWithDifferentYCoordinate()
+	{
+		Triplet t1 = new TestableTriplet(1, 2, 3);
+		Triplet t2 = new TestableTriplet(1, -2, 3);
+		assertThat(t1, not(t2));
+	}
+
+	@Test
+	public void tripletDoesNotEqualTripletWithDifferentZCoordinate()
+	{
+		Triplet t1 = new TestableTriplet(1, 2, 3);
+		Triplet t2 = new TestableTriplet(1, 2, -3);
+		assertThat(t1, not(t2));
+	}
+
+	class TestableTriplet extends Triplet
+	{
+
+		protected TestableTriplet(double x, double y, double z)
+		{
+			super(x, y, z, "TT");
+		}
+
+		protected TestableTriplet(double x, double y, double z, String shorthand)
+		{
+			super(x, y, z, shorthand);
+		}
+
+	}
+}
